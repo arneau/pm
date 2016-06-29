460db8f1053ec08641f8358d34a0f0454c8dca38
Arneau van der Horst <arneau@onedayonly.co.za>
2016-06-29 16:37:51 +0200

    Latest commit

--- index.php
+++ index.php
@@ -1,13 +1,37 @@
 <?
 
-$hours_per_day = 6;
+$hours_width = 25;
+$hours_gutter = 2;
+$hours_per_day = 8;
+
 $months_to_display = [
 	'July',
 	'August',
 ];
 
-?>
+$developers_tasks = [
+	'Niko' => [
+		[
+			'colour' => 'red',
+			'hours_estimated' => 4,
+			'hours_taken' => 4,
+			'description' => 'Do something',
+		],
+		[
+			'colour' => 'red',
+			'hours_estimated' => 6,
+			'hours_taken' => 8,
+			'description' => 'Do something else',
+		],
+		[
+			'colour' => 'dark_blue',
+			'hours_estimated' => 2,
+			'description' => 'Do nothing',
+		],
+	],
+];
 
+?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
 <head>
@@ -17,67 +41,93 @@ $months_to_display = [
 <body>
 <div class="project">
 	<div class="timeline">
+		<div class="months">
 
-		<?
+			<?
 
-		foreach ($months_to_display as $month) {
+			foreach ($months_to_display as $month) {
 
-			echo <<<s
-		<div class="month">
-			<h6 class="name">{$month}</h6>
-			<div class="days">
+				echo <<<s
+			<div class="month">
+				<h6 class="name">{$month}</h6>
+				<div class="days">
 s;
 
-			$days_in_month = date('t', strtotime($month . ' 2016')) * 1;
+				$days_in_month = date('t', strtotime($month . ' 2016')) * 1;
 
-			for ($date = 1; $date <= $days_in_month; $date ++) {
+				for ($date = 1; $date <= $days_in_month; $date++) {
 
-				$day_object = new DateTime($date . ' ' . $month . ' 2016');
+					$day_object = new DateTime($date . ' ' . $month . ' 2016');
 
-				if (!in_array($day_object->format('w'), [
-					0,
-					6,
-				])
-				) {
+					if (!in_array($day_object->format('w'), [
+						0,
+						6,
+					])
+					) {
 
-					$day_name = strtolower($day_object->format('l'));
+						$day_name = strtolower($day_object->format('l'));
 
-					echo <<<s
-					<div class="day {$day_name}">
-						<h6 class="name">
-							{$day_object->format('d')}
-							<span>({$day_object->format('D')})</span>
-						</h6>
-						<div class="hours">
-							<div class="hour"></div>
-							<div class="hour"></div>
-							<div class="hour"></div>
-							<div class="hour"></div>
-							<div class="hour"></div>
-							<div class="hour"></div>
+						echo <<<s
+						<div class="day {$day_name}">
+							<h6 class="name">
+								{$day_object->format('d')}
+								<span>({$day_object->format('D')})</span>
+							</h6>
+							<div class="hours">
+s;
+
+						echo str_repeat('<div class="hour"></div>', $hours_per_day);
+
+						echo <<<s
+							</div>
 						</div>
-					</div>
 s;
+
+					}
+
 				}
+
+				echo <<<s
+				</div>
+			</div>
+s;
 
 			}
 
-			echo <<<s
-			</div>
+			?>
+
 		</div>
+		<div class="developers">
+
+			<?
+
+			foreach ($developers_tasks as $developer_name => $developer_tasks) {
+
+				echo <<<s
+			<div class="developer">
+				<h2 class="name">{$developer_name}</h2>
+				<div class="tasks">
 s;
 
-		}
+				foreach ($developer_tasks as $developer_task_data) {
 
-		?>
+					$developer_task_width = $hours_width * $developer_task_data['hours'] + $hours_gutter * ($developer_task_data['hours'] - 1);
 
-	</div>
-	<div class="developer">
-		<h2 class="name">Niko</h2>
-		<div class="tasks">
-			<div class="task red" style="width: 80px;">Task #1</div>
-			<div class="task dark_blue" style="width: 80px;">Task #2</div>
-			<div class="task dark_green" style="width: 80px;">Task #3</div>
+					echo <<<s
+					<div class="task {$developer_task_data['colour']}" style="width: {$developer_task_width}px;" title="{$developer_task_data['description']}">{$developer_task_data['description']}</div>
+s;
+
+				}
+
+				echo <<<s
+				</div>
+			</div>
+s;
+
+			}
+
+			?>
+
 		</div>
 	</div>
 </div>
