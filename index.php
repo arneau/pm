b073212a266c883e93896f697fe3b2086782cbc6
Arneau van der Horst <github@arneau.me>
2016-06-29 10:21:06 +0200

    Latest update

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<?
+
+$hours_per_day = 6;
+$months_to_display = [
+	'July',
+	'August',
+];
+
+?>
+
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
 <head>
@@ -10,46 +20,65 @@
 
 		<?
 
-		$months = [
-			'July',
-			'August',
-		];
+		foreach ($months_to_display as $month) {
 
-		foreach ($months as $month) {
+			echo <<<s
+		<div class="month">
+			<h6 class="name">{$month}</h6>
+			<div class="days">
+s;
 
-			?>
+			$days_in_month = date('t', strtotime($month . ' 2016')) * 1;
 
-			<div class="month">
-				<h2 class="name"><? echo $month; ?></h2>
-				<div class="days">
+			for ($date = 1; $date <= $days_in_month; $date ++) {
 
-					<?
+				$day_object = new DateTime($date . ' ' . $month . ' 2016');
 
-					$days_in_month = date('t', strtotime($month . ' 2016')) * 1;
+				if (!in_array($day_object->format('w'), [
+					0,
+					6,
+				])
+				) {
 
-					for ($date = 1; $date <= $days_in_month; $date++) {
+					$day_name = strtolower($day_object->format('l'));
 
-						$day = new DateTime($date . ' ' . $month . ' 2016');
+					echo <<<s
+					<div class="day {$day_name}">
+						<h6 class="name">
+							{$day_object->format('d')}
+							<span>({$day_object->format('D')})</span>
+						</h6>
+						<div class="hours">
+							<div class="hour"></div>
+							<div class="hour"></div>
+							<div class="hour"></div>
+							<div class="hour"></div>
+							<div class="hour"></div>
+							<div class="hour"></div>
+						</div>
+					</div>
+s;
+				}
 
-						if ($day->format('w') == 0) {
-							echo '<div class="divider"></div>';
-						} elseif ($day->format('w') != 6) {
-							echo '<div class="day">' . $day->format('d') . ' <small>(' . $day->format('D') . ')</small></div>';
-						}
+			}
 
-					}
-
-					?>
-
-				</div>
+			echo <<<s
 			</div>
-
-			<?
+		</div>
+s;
 
 		}
 
 		?>
 
+	</div>
+	<div class="developer">
+		<h2 class="name">Niko</h2>
+		<div class="tasks">
+			<div class="task red" style="width: 80px;">Task #1</div>
+			<div class="task dark_blue" style="width: 80px;">Task #2</div>
+			<div class="task dark_green" style="width: 80px;">Task #3</div>
+		</div>
 	</div>
 </div>
 </body>
